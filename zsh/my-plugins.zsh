5c06f40fae50f536a314fd26bcba668da50e9610
Jeff Capone <jeffcap1@gmail.com>
2022-02-01 22:51:56 -0500

    fix(nvm): now works for both x86 and M1 Macs

--- zsh/my-plugins.zsh
+++ zsh/my-plugins.zsh
@@ -15,8 +15,8 @@ fi
 
 ### Starting Node Version Manager (nvm)
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Adding global require support -- Sublime Text and other apps
 export NODE_PATH="$(npm config get prefix)/lib/node_modules"
