bac9d80e19fa4866475f801cbeff8544a54ced67
Jeff Capone <jeffcap1@gmail.com>
2024-03-26 23:23:16 -0400

    :wrench: chore: Fixing keybinding issue and tmux status bar color

---
+++ zsh/my-keybindings.zsh
@@ -0,0 +1,7 @@
+bindkey -e
+bindkey '^[[1;3C' forward-word
+bindkey '^[[1;3D' backward-word
+bindkey '^[[D' beginning-of-line
+bindkey '^[[C' end-of-line
+
+
--- zsh/my-plugins.zsh
+++ zsh/my-plugins.zsh
@@ -21,35 +21,11 @@ export NVM_LAZY_LOAD=true
 # export NVM_LAZY_LOAD_EXTRA_COMMANDS=('cd','ls')
 source ~/.zsh-config-settings/plugins/zsh-nvm-plugin.zsh
 
-### Starting Node Version Manager (nvm)
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "/usr/local/opt/nvm/nvm.sh" ] && export NVM_BASE="/usr/local/opt/nvm" || export NVM_BASE="/opt/homebrew/opt/nvm"
-# [ -s "$NVM_BASE/nvm.sh" ] && \. "$NVM_BASE/nvm.sh"  # This loads nvm
-# [ -s "$NVM_BASE/bash_completion" ] && \. "$NVM_BASE/bash_completion"  # This loads nvm bash_completion
-#
-# # Adding global require support -- Sublime Text and other apps
-# export NODE_PATH="$(npm config get prefix)/lib/node_modules"
-#
-# # place this after nvm initialization!
-# autoload -U add-zsh-hook
-# load-nvmrc() {
-#   local node_version="$(nvm version)"
-#   local nvmrc_path="$(nvm_find_nvmrc)"
-#
-#   if [ -n "$nvmrc_path" ]; then
-#     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-#
-#     if [ "$nvmrc_node_version" = "N/A" ]; then
-#       nvm install
-#     elif [ "$nvmrc_node_version" != "$node_version" ]; then
-#       nvm use
-#     fi
-#   elif [ "$node_version" != "$(nvm version default)" ]; then
-#     echo "Reverting to nvm default version"
-#     nvm use default
-#   fi
-# }
-# add-zsh-hook chpwd load-nvmrc
-# load-nvmrc
-# ### Ending nvm
-#
+
+if [ ! -d ~/.tmuxifier ]; then
+  echo "Cloning tmuxifier..."
+  git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+fi
+export PATH="$HOME/.tmuxifier/bin:$PATH"
+eval "$(tmuxifier init -)"
+
