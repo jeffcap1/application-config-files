325f4a2712e8f5df6ef24478d75922fa88b26fdd
jeffcap1 <jeffcap1@gmail.com>
2024-04-01 09:37:03 -0400

    :bug: fix: Adjusting keybindings and settings for minor issues

--- zsh/my-keybindings.zsh
+++ zsh/my-keybindings.zsh
@@ -1,7 +1,7 @@
 bindkey -e
 bindkey '^[[1;3C' forward-word
 bindkey '^[[1;3D' backward-word
-bindkey '^[[D' beginning-of-line
-bindkey '^[[C' end-of-line
+bindkey '^[[1;5D' beginning-of-line
+bindkey '^[[1;5C' end-of-line
 
 
--- zsh/my-plugins.zsh
+++ zsh/my-plugins.zsh
@@ -18,7 +18,7 @@ fi
 export NVM_AUTO_USE=true
 export NVM_COMPLETION=true
 export NVM_LAZY_LOAD=true
-# export NVM_LAZY_LOAD_EXTRA_COMMANDS=('cd','ls')
+export NVM_LAZY_LOAD_EXTRA_COMMANDS=('cd','ls')
 source ~/.zsh-config-settings/plugins/zsh-nvm-plugin.zsh
 
 
