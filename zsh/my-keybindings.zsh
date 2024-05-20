73a9c110186c5a12bf4138bb145a8fbccf22e84b
jeffcap1 <jeffcap1@gmail.com>
2024-05-20 15:18:49 -0400

    :sparkles: feat: Better settings for zsh

--- zsh/my-keybindings.zsh
+++ zsh/my-keybindings.zsh
@@ -1,4 +1,6 @@
 bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
 bindkey '^[[1;3C' forward-word
 bindkey '^[[1;3D' backward-word
 bindkey '^[[1;5D' beginning-of-line
---
+++ zsh/my-settings.zsh
@@ -0,0 +1,16 @@
+# history settings
+HISTSIZE=10000
+SAVEHIST=$HISTSIZE
+HISTFILE=~/.zsh_history
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_find_no_dups
+
+# Completion style
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
