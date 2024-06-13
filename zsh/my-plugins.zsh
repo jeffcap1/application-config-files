6afc6b94c541ae6de091154953462dc2d4f6520c
jeffcap1 <jeffcap1@gmail.com>
2024-06-13 15:28:13 -0400

    :sparkles: feat: Improved FZF menu

--- zsh/my-plugins.zsh
+++ zsh/my-plugins.zsh
@@ -15,12 +15,6 @@ fi
 ### Ending colorls
 
 
-# export NVM_AUTO_USE=true
-# export NVM_COMPLETION=true
-# export NVM_LAZY_LOAD=true
-# export NVM_LAZY_LOAD_EXTRA_COMMANDS=('cd','ls')
-# source ~/.zsh-config-settings/plugins/zsh-nvm-plugin.zsh
-
 export ZSH_FNM_INSTALL_DIR="$HOME/.fnm"
 export ZSH_FNM_ENV_EXTRA_ARGS="--use-on-cd"
 export ZSH_FNM_USE_EXTRA_ARGS="--install-if-missing"
@@ -36,12 +30,41 @@ eval "$(tmuxifier init -)"
 
 # fzf
 
+# Default settigns
+export FZF_DEFAULT_OPT="--height 40% --layout reverse --info inline --border \
+    --bind 'ctrl-/:change-preview-window(50%|hidden|)' \
+"
+
 # Options to fzf command
 export FZF_COMPLETION_OPTS="--height 40% --layout reverse --info inline --border \
     --preview 'bat {}' --preview-window up,1,border-horizontal \
     --bind 'ctrl-/:change-preview-window(50%|hidden|)' \
-    --color 'fg:#bbccdd,fg+:#ddeeff,bg:#334455,preview-bg:#223344,border:#778899'\
-"
+    --color header:italic \
+    --header 'Compeletion Menu'"
+
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS=" \
+  --preview 'echo {}' --preview-window up:3:hidden:wrap \
+  --bind 'ctrl-/:toggle-preview' \
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' \
+  --color header:italic \
+  --header 'Press CTRL-Y to copy command into clipboard'"
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
+    export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
+  esac
+}
 
 if [ -x "$(command -v fzf)" ]; then
   eval "$(fzf --zsh)"
