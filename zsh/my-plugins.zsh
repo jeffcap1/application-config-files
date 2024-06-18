7efd371847677e1bacb50e46fa27af55dd6c39d2
Jeff Capone <jeffcap1@gmail.com>
2024-06-17 20:41:23 -0400

    :sparkles: feat: Replacing colorls with eza

--- zsh/my-plugins.zsh
+++ zsh/my-plugins.zsh
@@ -1,26 +1,16 @@
-### Starting colorls
-# Adding Ruby Gems to PATH
-# export PATH=$PATH:/usr/local/opt/ruby/bin:$HOME/.gem/ruby/3.0.0/bin
-export PATH="$(brew --prefix)/opt/ruby/bin:$PATH"
-
-# Ruby Config for compilers
-export LDFLAGS="-L$(brew --prefix)/opt/ruby/lib"
-export CPPFLAGS="-I$(brew --prefix)/opt/ruby/include"
-
-# Colorize ls output
-if [ -x "$(command -v colorls)" ]; then
-  alias ls="colorls"
-  alias la="colorls -al"
+# Eza
+if [ -x "$(command -v eza)" ]; then
+  alias ls="eza --color=always --icons=always"
+  alias la="eza -al --color=always --icons=always"
 fi
-### Ending colorls
 
-
+# fnm -- Node.js version manager
 export ZSH_FNM_INSTALL_DIR="$HOME/.fnm"
 export ZSH_FNM_ENV_EXTRA_ARGS="--use-on-cd"
 export ZSH_FNM_USE_EXTRA_ARGS="--install-if-missing"
 source ~/.zsh-config-settings/plugins/zsh-fnm-plugin.zsh
 
-
+# tmuxifier
 if [ ! -d ~/.tmuxifier ]; then
   echo "Cloning tmuxifier..."
   git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
