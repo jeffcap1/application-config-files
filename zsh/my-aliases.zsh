39311b647f11603fb9dfef5cb041c275fa32bd8e
Jeff Capone <jeffcap1@gmail.com>
2025-04-24 13:12:36 -0400

    :sparkles: feat: adding alias for pnpm

--- zsh/my-aliases.zsh
+++ zsh/my-aliases.zsh
@@ -9,6 +9,7 @@ alias addkey='eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa'
 alias vi="nvim"
 alias ff="fastfetch"
 alias lg="lazygit"
+alias p="pnpm"
 
 alias gt="git tag -l | sort -Vr | head -n 10"
 alias gtf="git tag -l | sort -Vr"
